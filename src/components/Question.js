ebba783530c806f63dd3515339b49962018b0547
Brian Lambert <brianlambert2408@gmail.com>
2022-10-20 11:32:41 +1100

    Completed tasks

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+    let timer = setTimeout (()=>{
+
+      // console.log(timeRemaining)
+      setTimeRemaining(time=> --time);
+
+      if(timeRemaining <=0){
+        handleAnswer(false);
+      }
+      
+    },1000);
+
+    return ()=>clearTimeout(timer);
+
+  });
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
